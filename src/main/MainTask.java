071d9172156627ef42da5170cd6d1500961a3487
yysdp <884319834@qq.com>
2020-10-07 10:44:32 +0800

    作业

---
+++ src/main/MainTask.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.ArrayList;
+
+public class MainTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList arr=new ArrayList();
+		arr.add(50);
+		arr.add(60);
+		arr.add(20);
+		arr.add(90);
+		arr.add(30);
+		//1,对成绩进行排序
+		//2,求平均分，最大值最小值
+		//3,统计区间：0-20：xx个，21-40：xx个
+		//
+	}
+
+}
